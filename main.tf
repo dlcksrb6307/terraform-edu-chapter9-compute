5de2451af08bdae3c4f9f23a27d270fd49356940
dlcksrb6307 <dlcksrb6307@lguplus.co.kr>
2022-10-21 11:01:55 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,141 @@
+terraform {
+  cloud {
+    organization = "tomandjerry-changyu"
+    hostname     = "app.terraform.io" # default
+
+    workspaces {
+      name = "terraform-edu-chapter9-compute"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      Project = "Coffee-Mug-Cake"
+      Owner   = "jerry & tom"
+    }
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "remote"
+
+  config = {
+    organization = "tomandjerry-changyu"
+    workspaces = {
+      name = "terraform-edu-chapter9-network"
+    }
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_eip" "hashicat" {
+  instance = aws_instance.hashicat.id
+  vpc      = true
+}
+
+resource "aws_eip_association" "hashicat" {
+  instance_id   = aws_instance.hashicat.id
+  allocation_id = aws_eip.hashicat.id
+}
+
+resource "aws_instance" "hashicat" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.hashicat.key_name
+  associate_public_ip_address = true
+  subnet_id                   = data.terraform_remote_state.network.outputs.subnet_id_hashicat
+  vpc_security_group_ids      = [data.terraform_remote_state.network.outputs.security_group_id_hashicat]
+
+  tags = {
+    Name = "${var.prefix}-hashicat-instance"
+  }
+}
+
+# We're using a little trick here so we can run the provisioner without
+# destroying the VM. Do not do this in production.
+
+# If you need ongoing management (Day N) of your virtual machines a tool such
+# as Chef or Puppet is a better choice. These tools track the state of
+# individual files and can keep them in the correct configuration.
+
+# Here we do the following steps:
+# Sync everything in files/ to the remote VM.
+# Set up some environment variables for our script.
+# Add execute permissions to our scripts.
+# Run the deploy_app.sh script.
+resource "null_resource" "configure_cat_app" {
+  depends_on = [aws_eip_association.hashicat]
+  // triggers = {
+  //   build_number = timestamp()
+  // }
+
+  provisioner "file" {
+    source      = "files/"
+    destination = "/home/ubuntu/"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = tls_private_key.hashicat.private_key_pem
+      host        = aws_eip.hashicat.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt -y update",
+      "sleep 15",
+      "sudo apt -y update",
+      "sudo apt -y install apache2",
+      "sudo systemctl start apache2",
+      "sudo chown -R ubuntu:ubuntu /var/www/html",
+      "chmod +x *.sh",
+      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
+      "sudo apt -y install cowsay",
+      "cowsay Mooooooooooo!",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = tls_private_key.hashicat.private_key_pem
+      host        = aws_eip.hashicat.public_ip
+    }
+  }
+}
+
+resource "tls_private_key" "hashicat" {
+  algorithm = "RSA"
+}
+
+locals {
+  private_key_filename = "${var.prefix}-ssh-key.pem"
+}
+
+resource "aws_key_pair" "hashicat" {
+  key_name   = local.private_key_filename
+  public_key = tls_private_key.hashicat.public_key_openssh
+}
